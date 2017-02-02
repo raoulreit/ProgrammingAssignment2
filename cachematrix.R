462dfe587b2569b70830491d7c21fcd53ce9c6d7
raoulreit <raoulreit@gmail.com>
2017-02-02 14:05:06 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
 ##R-Programming - Assignment 2 - Lexical Scoping 
 
 ## The following functions will create a special matrix and will calculate the inverse of that matrix
-## by caching it if it has already been calculated
+## by caching it if it has already been calculated.
 
 
-## This function creates a special matrix, which in essence is a list
+## This function creates a special matrix, which in essence is a list containing 4 functions that: 
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
           
@@ -20,7 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This functional caches the inverse of the special matrix
+## This function uses the output of the function above, cacheSolve(makeCacheMatrix(x)), to:
+## return the inverse of the matrix from the cache if it has already been calculated
+## calculate the inverse if not
 
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
